a8965180d3eac0d09a0227d12b4da85cb5d0580d
Kim Lindberg Schwaner <kicw@dgs-diagnostics.com>
2024-11-09 23:00:06 +0100

    Update example with shortened algorithms and std::ranges example

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <ranges>
 
 #include "collection.hpp"
 
@@ -16,71 +17,79 @@ int main()
     std::cout << item << std::endl;
   };
 
-  int value = collection
-                  .each(print)
-                  .each([](auto&& item) {
-                    if (item % 2 == 0)
-                    {
-                      item *= 2;
-                    }
-                  })
-                  .each(print)
-                  .sort()
-                  .each(print)
-                  .sort([](auto&& a, auto&& b) {
-                    return a > b;
-                  })
-                  .each(print)
-                  .get(0);
-
-  std::cout << value << std::endl;
-  std::cout << "Size: " << collection.size() << std::endl;
-  std::cout << collection.pluck(0) << std::endl;
-  std::cout << "Size: " << collection.size() << std::endl;
-
-  std::vector<int> vector{};
-
-  std::copy(data.begin(), data.end(), std::back_insert_iterator{vector});
-
-  std::cout << "Size: " << vector.size() << std::endl;
-  for (auto&& item : vector)
+  // With method chaining
   {
-    std::cout << item << std::endl;
+    int value = collection
+                    .each(print)
+                    .each([](auto&& item) {
+                      if (item % 2 == 0)
+                      {
+                        item *= 2;
+                      }
+                    })
+                    .each(print)
+                    .sort()
+                    .each(print)
+                    .sort([](auto&& a, auto&& b) {
+                      return a > b;
+                    })
+                    .each(print)
+                    .get(0);
+
+    std::cout << value << std::endl;
+    std::cout << "Size: " << collection.size() << std::endl;
+    // The below pluck() is a problem regarding exception safety if
+    // decltype(collection)::value_type's ctor throws! This is the
+    // reason why vector::pop_front() doesn't return the popped value.
+    std::cout << collection.pluck(0) << std::endl;
+    std::cout << "Size: " << collection.size() << std::endl;
   }
 
-  for (auto&& item : vector)
+  // With regular <algorithm>s
   {
-    if (item % 2 == 0)
-    {
-      item *= 2;
-    }
+    std::vector<int> vector{data.begin(), data.end()};
+    std::for_each(vector.begin(), vector.end(), print);
+    std::for_each(vector.begin(), vector.end(), [](auto&& item) {
+      if (item % 2 == 0)
+      {
+        item *= 2;
+      }
+    });
+    std::for_each(vector.begin(), vector.end(), print);
+    std::sort(vector.begin(), vector.end(), std::greater<int>{});
+    std::for_each(vector.begin(), vector.end(), print);
+    int value = vector[0];
+
+    std::cout << value << std::endl;
+    std::cout << "Size: " << vector.size() << std::endl;
+    std::cout << vector.at(0) << std::endl; // or vector.front()
+    vector.erase(vector.begin());           // or vector.pop_front()?
+    std::cout << "Size: " << vector.size() << std::endl;
   }
 
-  for (auto&& item : vector)
+  // With <ranges>
   {
-    std::cout << item << std::endl;
+    namespace rng = std::ranges;
+    std::vector<int> vector{data.begin(), data.end()};
+    // With newer GCC, vector can also be constructed like so:
+    //   - std::vector<int> vector{std::from_range, data}; or
+    //   - auto vector = std::ranges::to<std::vector>(data);
+    rng::for_each(vector, print);
+    rng::for_each(vector, [](auto&& item) {
+      if (item % 2 == 0)
+      {
+        item *= 2;
+      }
+    });
+    rng::for_each(vector, print);
+    rng::sort(vector, std::greater<int>{});
+    rng::for_each(vector, print);
+    int value = vector[0];
+
+    std::cout << value << std::endl;
+    std::cout << "Size: " << vector.size() << std::endl;
+    std::cout << vector.at(0) << std::endl; // or vector.front()
+    vector.erase(vector.begin());           // or vector.pop_front()?
+    std::cout << "Size: " << vector.size() << std::endl;
   }
-
-  std::sort(vector.begin(), vector.end());
-
-  for (auto&& item : vector)
-  {
-    std::cout << item << std::endl;
-  }
-
-  std::sort(vector.begin(), vector.end(), [](auto&& a, auto&& b) {
-    return a > b;
-  });
-
-  for (auto&& item : vector)
-  {
-    std::cout << item << std::endl;
-  }
-
-  std::cout << vector.at(0) << std::endl;
-
-  std::cout << "Size: " << vector.size() << std::endl;
-  std::cout << vector.at(0) << std::endl;
-  vector.erase(vector.begin());
-  std::cout << "Size: " << vector.size() << std::endl;
 }
